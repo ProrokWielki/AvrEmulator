6e181710ea178530bb45786cedd1ef61c0b9fe95
Paweł Warzecha <ProrokWielki@o2.pl>
2024-10-13 09:49:25 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 // #[path = "instruction/nop.rs"]
-#[path = "instruction/add.rs"]
-mod add;
+// #[path = "instruction/add.rs"]
+// mod add;
 
-mod memory;
+// mod memory;
 // mod nop;
 
 fn main() {
     println!("Hello world!");
     // let mut _inst = nop::NOP::new();
-    let mut _inst2 = add::ADD::new(0);
+    // let mut _inst2 = add::ADD::new(0);
 }
