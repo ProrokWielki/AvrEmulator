4b679f56506a3ed0113769c14decbb7163cb396a
ProrokWielki <ProrokWielki@o2.pl>
2024-10-13 07:50:50 +0000

    Added interface for instuructions

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,6 +1,25 @@
-mod add;
+use crate::registers::Registers;
 
 pub trait Instruction {
-    fn eq(rhs: u16) -> Bool;
-    fn perform(self, memory: Memory);
+    fn process(&self, registers: &mut Registers) -> ();
+    fn str(&self) -> String;
+
+    fn get_instruction_codes() -> Vec<u16>
+    where
+        Self: Sized;
+    fn get_instruction_mask() -> u16
+    where
+        Self: Sized;
+
+    fn eq(opcode: u16) -> bool
+    where
+        Self: Sized,
+    {
+        for instcruction_code in Self::get_instruction_codes() {
+            if opcode & Self::get_instruction_mask() == instcruction_code {
+                return true;
+            }
+        }
+        false
+    }
 }
